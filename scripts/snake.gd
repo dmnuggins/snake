d6181384ca1b6090f54be8441dd298cf3d7e0c62
dylan <7257923+dmnuggins@users.noreply.github.com>
2024-07-28 15:24:25 -0700

    Add world boundary and movement fixed to axis
    - Set world boundaries on edge of screen to prevent player from going out of bounds
    - Updated Player script to limit movement to x/y-axis
    - Player continues to move in the direction of the last pressed action
    - Movement is handled with velocity change instead of position change
    - Function move_and_slide() used to ensure interaction with screen edge colliders

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -3,11 +3,23 @@ extends CharacterBody2D
 @export var speed = 300.0
 
 func _physics_process(delta):
+	
+	# Player will move in the direction of the respective action pressed
 	if Input.is_action_pressed("up"):
-		position.y -= speed * delta
-	if Input.is_action_pressed("down"):
-		position.y += speed * delta
-	if Input.is_action_pressed("left"):
-		position.x -= speed * delta
-	if Input.is_action_pressed("right"):
-		position.x += speed * delta
+		velocity.y -= speed * delta
+		velocity.x = 0
+	elif Input.is_action_pressed("down"):
+		velocity.y += speed * delta
+		velocity.x = 0
+	elif Input.is_action_pressed("left"):
+		velocity.x -= speed * delta
+		velocity.y = 0
+	elif Input.is_action_pressed("right"):
+		velocity.x += speed * delta
+		velocity.y = 0
+	
+	# Sets the player velocity
+	velocity = velocity.normalized() * speed
+	
+	# Ensures that player interacts with world boundaries
+	move_and_slide()
