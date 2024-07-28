7c2c456f5bc8f651db10a5eb59c66ea4e425440a
dylan <7257923+dmnuggins@users.noreply.github.com>
2024-07-28 16:39:04 -0700

    Fixed movement to grid alignment and wall angle
    .gitignore:
    - Added *.tmp to ignore temp files generated by Godot
    
    game.tscn:
    - Fixed LeftWall angle to be 90 degrees, was 89.9 degrees
    
    snake.gd:
    - Function checkAlignment() checks if position of player is in line with set grid size
    - Updated move_and_slide() to move_and_collide(velocity) to prevent player from continuing to slide after colliding with walls

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
 
-@export var speed = 300.0
+@export var speed = 100.0
 
 func _physics_process(delta):
 	
 	# Player will move in the direction of the respective action pressed
-	if Input.is_action_pressed("up"):
-		velocity.y -= speed * delta
+	if Input.is_action_pressed("up") && checkAlignment(position):
+		velocity.y -= 1
 		velocity.x = 0
-	elif Input.is_action_pressed("down"):
-		velocity.y += speed * delta
+	elif Input.is_action_pressed("down") && checkAlignment(position):
+		velocity.y += 1
 		velocity.x = 0
-	elif Input.is_action_pressed("left"):
-		velocity.x -= speed * delta
+	elif Input.is_action_pressed("left") && checkAlignment(position):
+		velocity.x -= 1
 		velocity.y = 0
-	elif Input.is_action_pressed("right"):
-		velocity.x += speed * delta
+	elif Input.is_action_pressed("right") && checkAlignment(position):
+		velocity.x += 1
 		velocity.y = 0
 	
 	# Sets the player velocity
-	velocity = velocity.normalized() * speed
+	velocity = velocity.normalized() * speed * delta
 	
 	# Ensures that player interacts with world boundaries
-	move_and_slide()
+	move_and_collide(velocity)
+	
+	position = Vector2(round(position.x), round(position.y))
+	
+	if checkAlignment(position):
+		print(position)
+
+# Check if player is aligned with grid given current position and square size
+func checkAlignment(position):
+	var x = int(position.x)
+	var y = int(position.y)
+	
+	if (x % 20 == 0) && (y % 20 == 0):
+		return true
+	return false
