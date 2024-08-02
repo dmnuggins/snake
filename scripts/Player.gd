0a20584a5203cb2e600b7b0937e8b47ef36c222e
dylan <7257923+dmnuggins@users.noreply.github.com>
2024-08-01 22:56:55 -0700

    Refactored scripts + scenes, initialize Player finite state machine

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -20,16 +20,16 @@ var next_direction
 func _physics_process(delta):
 	
 	# Player will move in the direction of the respective action pressed
-	if Input.is_action_pressed("up") && checkAlignment(position) && (current_direction != MoveType.VERTICAL):
+	if Input.is_action_pressed("up") && checkAlignment() && (current_direction != MoveType.VERTICAL):
 		velocity = Vector2(0,-1)
 		current_direction = MoveType.VERTICAL
-	elif Input.is_action_pressed("down") && checkAlignment(position) && (current_direction != MoveType.VERTICAL):
+	elif Input.is_action_pressed("down") && checkAlignment() && (current_direction != MoveType.VERTICAL):
 		velocity = Vector2(0,1)
 		current_direction = MoveType.VERTICAL
-	elif Input.is_action_pressed("left") && checkAlignment(position) && (current_direction != MoveType.HORIZONTAL):
+	elif Input.is_action_pressed("left") && checkAlignment() && (current_direction != MoveType.HORIZONTAL):
 		velocity = Vector2(-1,0)
 		current_direction = MoveType.HORIZONTAL
-	elif Input.is_action_pressed("right") && checkAlignment(position) && (current_direction != MoveType.HORIZONTAL):
+	elif Input.is_action_pressed("right") && checkAlignment() && (current_direction != MoveType.HORIZONTAL):
 		velocity = Vector2(1,0)
 		current_direction = MoveType.HORIZONTAL
 		
@@ -43,7 +43,7 @@ func _physics_process(delta):
 	
 	position = Vector2(round(position.x), round(position.y))
 	
-	if checkAlignment(position):
+	if checkAlignment():
 		print(position)
 
 # Changes the direction of the snake if aligned
@@ -51,7 +51,7 @@ func changeDirection():
 	pass
 
 # Check if player is aligned with grid given current position and square size
-func checkAlignment(position):
+func checkAlignment():
 	var x = int(position.x)
 	var y = int(position.y)
 	
