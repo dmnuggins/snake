3c5b66081929bcc9562e102c3c4a6a2ad16fce60
dylan <7257923+dmnuggins@users.noreply.github.com>
2024-08-06 21:10:01 -0700

    Modified project settings + initialized FSM code

--- scripts/FSM/States/idle.gd
+++ scripts/FSM/States/idle.gd
@@ -1,11 +1,9 @@
-extends Node
+extends PlayerState
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func enter(previous_state_path: String, data := {}) -> void:
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func physics_update(_delta: float) -> void:
 	pass
--- scripts/FSM/States/moving.gd
+++ scripts/FSM/States/moving.gd
@@ -1,11 +1,8 @@
-extends Node
+extends PlayerState
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func enter(previous_state_path: String, data := {}) -> void:
 	pass
+
+func physics_update(delta: float) -> void:
+	pass
+
